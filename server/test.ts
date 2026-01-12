9f71dcd4a95a6b9ab2e401d2e5b7039d5d67366b
kengps <106742004+kengps@users.noreply.github.com>
2026-01-12 13:30:23 +0700

    add Noti3

--- server/test.ts
+++ server/test.ts
@@ -4,15 +4,20 @@ interface note {
   count: number;
 }
 
+let Noti: note = {
+  name: "แจ้งเตือน",
+  description: "ราคาน้ำมัน",
+  count: 30,
+};
 
-let Noti:note ={
-    name: "แจ้งเตือน",
-    description: "ราคาน้ำมัน",
-    count: 30
-}
+let Noti2: note = {
+  name: "แจ้งเตือน2",
+  description: "ราคาน้ำมัน2",
+  count: 390,
+};
 
-let Noti2:note ={
-    name: "แจ้งเตือน2",
-    description: "ราคาน้ำมัน2",
-    count: 390
-}
+let Noti3: note = {
+  name: "แจ้งเตือน2",
+  description: "ราคาน้ำมัน2",
+  count: "390",
+};
